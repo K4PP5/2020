df1468769e24391eb0cb69e2b1251f7b49d3d86a
Kael Amir Pina Paraguassu Silva <kael.paraguassu@academico.ifrn.edu.br>
2020-02-28 17:47:09 -0300

    Exercício1

---
+++ Lista 6/Exercício1.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício1.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while (x <= 10)
+            {
+                Console.Write(x + " ");
+                x++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
