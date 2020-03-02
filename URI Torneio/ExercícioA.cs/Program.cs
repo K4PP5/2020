18fdb1ef0a044057cbe70d0fbf5c972b0e61879b
Kael Amir Pina Paraguassu Silva <kael.paraguassu@academico.ifrn.edu.br>
2020-03-02 15:15:25 -0300

    URI Torneio

---
+++ URI Torneio/ExercícioA.cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercícioA.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia = int.Parse(Console.ReadLine());
+            double combustivel = double.Parse(Console.ReadLine());
+            double consumo = distancia / combustivel;
+            Console.WriteLine($"{consumo.ToString("0.000")} km/l");
+            Console.ReadKey();
+
+        }
+    }
+}
