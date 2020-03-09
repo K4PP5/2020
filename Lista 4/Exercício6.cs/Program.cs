8d05411dee0da87f10d7dffb3fa65686b5707777
Kael Amir Pina Paraguassu Silva <kael.paraguassu@academico.ifrn.edu.br>
2020-03-09 15:13:20 -0300

    fim

---
+++ Lista 4/Exercício6.cs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício6.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o primeiro horário");
+            string[] tempo1 = Console.ReadLine().Split(' ');
+            string[] horario1 = tempo1[0].Split(':');
+
+            int horas1 = int.Parse(horario1[0]);
+            int minutos1 = int.Parse(horario1[1]);
+            string momento1 = tempo1[1];
+
+            Console.WriteLine("Informe o segundo horário");
+            string[] tempo2 = Console.ReadLine().Split(' ');
+            string[] horario2 = tempo1[0].Split(':');
+
+            int horas2 = int.Parse(horario2[0]);
+            int minutos2 = int.Parse(horario2[1]);
+            string momento2 = tempo2[1];
+
+            if (momento1 == "AM" && momento2 == "PM") Console.WriteLine($"O maior horário é {horario2}");
+            else if (momento1 == "PM" && momento2 == "AM") Console.WriteLine($"O maior horário é {horario1}");
+            else if (momento1 == momento2)
+            {
+                if (horas1 > horas2) Console.WriteLine($"O maior horário é {horario1}");
+                else if (horas2 > horas1) Console.WriteLine($"O maior horário é {horario2}");
+                else if (horas1 == horas2)
+                {
+                    if (minutos1 > minutos2) Console.WriteLine($"O maior horário é {horario1}");
+                    else if (minutos2 > minutos1) Console.WriteLine($"O maior horário é {horario2}");
+                }
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Lista 4/Exercício9.cs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício9.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("informe quatro valores inteiros diferentes");
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            int D = int.Parse(Console.ReadLine());
+
+            int somaTotal = A + B + C + D;
+            int maior = A;
+            int menor = A;
+            if (B > maior) maior = B;
+            if (C > maior) maior = C;
+            if (D > maior) maior = D;
+
+            if (B < menor) menor = B;
+            if (C < menor) menor = C;
+            if (D < menor) menor = D;
+
+            int soma2 = somaTotal - maior;
+
+
+
+
+        }
+    }
+}
